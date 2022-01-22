e1bb03bbf946072b3a2a0a2b955b676b70a84a35
Evan Walter <ewpw5678@gmail.com>
2022-01-22 13:20:15 -0800

    updated lat with setState

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.state = { lat: null };
+    
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
 
-    this.state = { lat: 40 };
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
     return <>Latitude: {this.state.lat}</>;
   }
 }
