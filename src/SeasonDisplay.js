3865351e64a1cbaa6f1420160ffade3d31453388
Evan Walter <ewpw5678@gmail.com>
2022-01-22 15:46:17 -0800

    determined season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export default function SeasonDisplay({ lat }) {
+  const getSeason = function (lat, month) {
+    if (month > 2 && month < 9) {
+      return lat > 0 ? 'summer' : 'winter';
+    }
+    else {
+      return lat > 0 ? 'winter' : 'summer';
+    }
+  };
+
+  const season = getSeason(lat, new Date().getMonth());
+  console.log(season);
+
   return <>Latitude: {lat}</>;
 }
