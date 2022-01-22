57d4c52f74c95336925a4f07afc34e9204e8e4d4
Evan Walter <ewpw5678@gmail.com>
2022-01-22 13:00:05 -0800

    finishing converting function component to class component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-
-  return <>Latitude: </>;
-}
-
 class App extends React.Component {
   render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
     return <>Latitude: </>;
   }
 }
